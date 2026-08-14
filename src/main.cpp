9c1b773ca59ea42e83a03655ac1a43fd08ff98f5
Deep-pdf <soumodeepsantra5@gmail.com>
2026-08-15 02:12:50 +0530

    greetings

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
+#include "mono.h"
+#include "MikodacsPCS8pt7b.h"
 
 #define TFT_CS   5
 #define TFT_DC   2
@@ -11,13 +13,16 @@ void setup() {
   tft.initR(INITR_BLACKTAB);
   tft.setRotation(2);
 
-  tft.fillScreen(ST77XX_BLACK);
+  tft.fillScreen(ST77XX_ORANGE);
 
-  tft.setTextColor(ST77XX_WHITE);
+  tft.setFont(&MikodacsPCS8pt7b);
+  tft.setTextColor(ST77XX_BLACK);
   tft.setTextSize(1);
-  tft.setCursor(30, 50);
-
-  tft.println("Hello World!");
+  
+  // Fits on a single line (approx 118px wide) on the 128px screen
+  tft.setCursor(5, 80);
+  tft.print("Hello");
+  tft.print(" DeadDeep");
 }
 
 void loop() {
